0bbd749e0025dae47b7a2a62d816fced8b139a3b
powerm17 <105803016+powerm17@users.noreply.github.com>
2024-02-19 14:05:25 -0500

    Split.java Updated for txt file
    Modified split code to read from a text file. The string split method was modified to split after each period. trim removes any unwanted spaces for print out.

--- split.java
+++ split.java
@@ -1,12 +1,23 @@
 // Java program to demonstrate working of split()
-public class split {
-	public static void main(String args[])
-	{
-		String str
-			= "GeeksforGeeks:A Computer Science Portal";
-		String[] arrOfStr = str.split(":");
 
-		for (String a : arrOfStr)
-			System.out.println(a);
-	}
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class split {
+    public static void main(String args[]) throws IOException {
+        String file = "C:\\Users\\wagne\\OneDrive\\Documents\\GitHub\\NLPpipeline-\\test.text";
+        try (BufferedReader reader = new BufferedReader(new FileReader(file))) {
+            String str = reader.readLine();
+            String[] arrOfStr = str.split("\\.");
+
+            for (String a : arrOfStr)
+                System.out.println(a.trim());
+
+            // Close reader
+            reader.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 }
